c5b11144b09a147c4f7120471fa8aaaaa1064fb8
Rohini Saravanakumar <rohiniskumar17@gmail.com>
2025-04-17 18:57:05 -0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Rohini Saravanakumar',
+    author_email= 'rohiniskumar17@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
